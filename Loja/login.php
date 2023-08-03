08741e43ef8c28902a376b826585a3903f919ed4
GuiVMolina <guilhermevieiramolina@gmail.com>
2023-08-03 08:41:31 -0300

    Login

---
+++ Loja/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login App</title>
+  <link href="Styles/Loginstyles.css" rel="stylesheet">
+</head>
+<body>
+<div class="navbar">
+ <img src="../hip.png">
+  <div class="container">
+    <div class="content">
+      <div class="title">Login</div>
+      <input class="input emailInput" type="text" placeholder="Email">
+      <input class="input passwordInput" type="password" placeholder="Password">
+      <div class="button">
+        <div class="buttonText">▶</div>
+      </div>
+      <div class="forgotPasswordText">Esqueceu sua senha?</div>
+      <div class="newAccount">Ainda não tem conta?</div>
+    </div>
+  </div>
+</body>
+</html>
